a7610fa4e1da28616242325461fa10e13320dfea
JinsuHong <jinsuhong.knight@gmail.com>
2026-05-30 03:28:28 +0000

    update f107 configs

--- scripts/finetuning/run_f107_experiments.py
+++ scripts/finetuning/run_f107_experiments.py
@@ -12,44 +12,50 @@ from sdofmv2.core import MAE, MAE_old
 from sdofmv2.tasks.f107 import EmbSolarProxyDataModule, MultiLayerPerceptron
 
 
-@hydra.main(config_path="../../configs/downstream", config_name="finetune_f107_config_sdofmv2_ALL.yaml")
+@hydra.main(
+    config_path="../../configs/downstream", config_name="finetune_f107_config_sdofmv2_ALL.yaml"
+)
 def main(cfg: DictConfig):
     logger.info("Starting F10.7 experiment...")
 
     # Setup DataModule
-    cache_dir = cfg.data.cache_dir
+
     train_index = Path(cfg.data.train_index).name
     val_index = Path(cfg.data.val_index).name
     test_index = Path(cfg.data.test_index).name
 
     datamodule = EmbSolarProxyDataModule(
-        hmi_path=os.path.join(cfg.data.sdoml.base_directory, cfg.data.sdoml.sub_directory.hmi)
-        if cfg.data.sdoml.sub_directory.hmi
-        else None,
-        aia_path=os.path.join(cfg.data.sdoml.base_directory, cfg.data.sdoml.sub_directory.aia)
-        if cfg.data.sdoml.sub_directory.aia
-        else None,
+        hmi_path=(
+            os.path.join(cfg.data.sdoml.base_directory, cfg.data.sdoml.sub_directory.hmi)
+            if cfg.data.sdoml.sub_directory.hmi
+            else None
+        ),
+        aia_path=(
+            os.path.join(cfg.data.sdoml.base_directory, cfg.data.sdoml.sub_directory.aia)
+            if cfg.data.sdoml.sub_directory.aia
+            else None
+        ),
         eve_path=None,
         components=cfg.data.sdoml.components,
         wavelengths=cfg.data.sdoml.wavelengths,
         ions=cfg.data.sdoml.ions,
         batch_size=cfg.model.misc.batch_size,
         num_workers=cfg.data.num_workers,
-        train_index=os.path.join(cache_dir, train_index),
-        val_index=os.path.join(cache_dir, val_index),
-        test_index=os.path.join(cache_dir, test_index),
+        train_index=cfg.data.train_index,
+        val_index=cfg.data.val_index,
+        test_index=cfg.data.test_index,
         num_frames=cfg.model.mae.num_frames,
         drop_frame_dim=cfg.data.drop_frame_dim,
         apply_mask=cfg.data.sdoml.apply_mask,
         precision=cfg.experiment.precision,
         normalization=cfg.data.sdoml.normalization,
-        normalization_stat_path=cache_dir,
+        normalization_stat_path=cfg.data.normalization_stat_path,
         ds_data_path=cfg.data.ds_data_path,
     )
     datamodule.setup()
 
     # Load Backbone
-    ckpt_path = cfg.model.backbone_checkpoint_path
+    ckpt_path = os.path.join(cfg.experiment.backbone.ckpt_dir, cfg.experiment.backbone.weight_name)
     if cfg.model.backbone_type == "MAE_old":
         backbone = MAE_old.load_from_checkpoint(
             checkpoint_path=ckpt_path,
@@ -77,10 +83,9 @@ def main(cfg: DictConfig):
         test_results_filename=cfg.experiment.test_results_filename,
     )
 
-
     # Trainer
     checkpoint_callback = ModelCheckpoint(
-        dirpath=cfg.experiment.output_dir,
+        dirpath=cfg.experiment.ds_ckpt_dir,
         filename=cfg.experiment.checkpoint_filename,
         monitor="val_loss",
         mode="min",
@@ -89,7 +94,7 @@ def main(cfg: DictConfig):
 
     trainer = l.Trainer(
         max_epochs=cfg.model.misc.epochs,
-        devices=[0] if torch.cuda.is_available() else "auto",
+        devices=cfg.experiment.distributed.devices,
         precision=cfg.experiment.precision,
         callbacks=[checkpoint_callback],
     )
